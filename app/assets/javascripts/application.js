2696676394647e3c4470c3ac49542edd389ac6d8
Layne McNish <lmcnish14@gmail.com>
2014-10-20 12:06:09 -0600

    finished all except for question counter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,33 +6,85 @@
 var questionNumber = 0;
 var score = 0;
 
-function getQuestions(){
-  $.ajax({
-    type:"GET",
-    url:"/quizzes/1/questions",
-    success: (function (questions) {
-      $.each(questions, function(i, question){
-        $('.question').append(question.description);
-        $.each(question.possible_answers, function(i, answer){
-          $('.answers').append('<li>' + answer.description + '</li>')
-        });
+function getQuestion() {
+  $.get("/quizzes/1/questions", function (data, i) {
+      $('h1').empty();
+      $('.submit').remove();
+      $('.answers').empty();
+      $('#next').remove();
+
+      $('.question').append('<h1>' + data[questionNumber].description + '</h1>');
+      $('.nav').append('<button class="btn btn-primary submit">Submit</button>');
+      $.each(data[questionNumber].possible_answers, function (i, answer) {
+        $('.answers').append('<button class="btn btn-default" value="' + answer.description + '" id="' + answer.description + '">' + answer.description + '</button>')
       })
+    }
+  );
+}
+
+
+function nextQuestion() {
+  $(document).on('click', "#next", function () {
+    var quizLength = 0;
+    $.get("/quizzes/1/questions", function (data) {
+      $.each(data, function () {
+        quizLength++;
+      });
+      console.log(questionNumber);
+      console.log(quizLength);
+      if (questionNumber < (quizLength-1)) {
+        questionNumber++;
+        getQuestion();
+      } else {
+        $('h1').empty();
+        $('.submit').remove();
+        $('.answers').empty();
+        $('#next').remove();
+        $('.score').append('<h1>Score:' + score + '</h1>')
+        console.log("finsihed")
+      }
+
     })
   })
 }
 
-function nextQuestion(){}
+function checkAnswer() {
 
-function checkAnswer(){}
+  $(document).on('click', ".btn-default", function () {
+    var button = this;
+    var answer = this.value;
+    $(document).on('click', ".submit", function () {
+      $.get("/quizzes/1/questions", function (data) {
+        $.each(data[questionNumber].possible_answers, function (i, ans) {
+          if (ans.description === answer) {
+            if (ans.correct === true) {
+              $(button).removeClass('btn-default').addClass('btn-success');
+              score++;
+              console.log(score);
+              $('.submit').remove();
+              $('#next').remove();
+              $('.nav').append('<button class="btn btn-primary" id="next">Next Question</button>')
+            }
+            else {
+              $(button).removeClass('btn-default').addClass('btn-danger');
+              $('.submit').remove();
+              $('#next').remove();
+              $('.nav').append('<button class="btn btn-primary" id="next">Next Question</button>')
+            }
 
-//Event handlers for the buttons
-//obj=document.getElementById("startq");
-//obj.onclick=getQuestions;
-//ans=document.getElementById("submit");
-//ans.onclick=checkAnswer;
+          }
+        });
+      });
 
-$(document).ready(function() {
+    });
+  });
+}
 
-  getQuestions();
+
+$(document).ready(function () {
+  getQuestion();
+  checkAnswer();
+  nextQuestion();
 
 });
+
